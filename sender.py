b07255f7d15b4fd6bbe0782a9217267d982bbd81
Yixiang Gao <yg5d6@mail.missouri.edu>
2023-09-08 23:31:18 -0500

    add sender

--- sender.py
+++ sender.py
@@ -1,14 +1,19 @@
+import time
+import random
 import asyncio
 import websockets
 
 async def handler(websocket):
   while True:
-    message = await websocket.recv()
+    message = str(random.randint(0,3)) 
     print(message)
+    await websocket.send(message)
+    time.sleep(0.1)
 
 async def main():
   async with websockets.serve(handler, "", 8001):
     await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
+  # input("Press Enter to continue...")
   asyncio.run(main())
